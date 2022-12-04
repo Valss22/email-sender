c1e306d4e75993bb0e91dd5f34b68fbd6ed03729
Valss22 <valsshokorov@gmail.com>
2022-12-04 14:03:27 +0600

    Remove unused code & requirements

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
-# type: ignore
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
-import subprocess
-import sys
-import uvicorn
-
-from app.graphql_client.route import graphql_client
 from app.user.graphql.query import graphql_app
 
 app = FastAPI()
@@ -21,7 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(graphql_client)
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
 
@@ -32,10 +25,3 @@ register_tortoise(
     generate_schemas=False,
     add_exception_handlers=True,
 )
-
-if __name__ == '__main__':
-    # status_output: tuple[int, str] = subprocess.getstatusoutput("mypy .")
-    # if status_output[0]:
-    #     print(status_output[1])
-    # else:
-    uvicorn.run(app, host="127.0.0.1", port=8000)
--- app/user/schemas.py
+++ app/user/schemas.py
@@ -1,12 +1,1 @@
 from pydantic import BaseModel
-
-
-class UserIn(BaseModel):
-    email: str
-    password: str
-
-
-class UserOut(BaseModel):
-    id: str
-    email: str
-    jwt: str
