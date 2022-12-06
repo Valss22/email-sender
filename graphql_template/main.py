caea498fce593c928325fee1d205e63a6a87132a
Valss22 <valsshokorov@gmail.com>
2022-12-07 01:43:24 +0600

    Delete graphql folder

--- graphql_template/main.py
+++ graphql_template/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from .users.graphql.query import user_graphql_router
+from .users.graphql import user_graphql_router
 
 app = FastAPI()
 
