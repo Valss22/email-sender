95da6d7308c681ad6c2502d6d0de115b258fc6eb
Valss22 <valsshokorov@gmail.com>
2022-12-07 01:25:40 +0600

    Add user query & mutation

--- graphql_template/users/graphql/query.py
+++ graphql_template/users/graphql/query.py
@@ -1,7 +1,7 @@
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 from strawberry.types import Info
-from graphql_template.users.services import create_user
+from graphql_template.users.services import create_user, get_users_list
 from graphql_template.users.schemas import UserCreate
 from graphql_template.dependencies import get_db_contex
 
@@ -14,12 +14,19 @@ class User:
 
 @strawberry.type
 class UserMutation:
-    @strawberry.field
+    @strawberry.mutation
     async def create_user(self, info: Info) -> None:
         user = UserCreate(name="test", age=10)
         await create_user(user, info.context["db"])
         return None
 
 
-user_schema = strawberry.Schema(UserMutation)
+@strawberry.type
+class UserQuery:
+    @strawberry.field
+    async def get_users_list(self, info: Info) -> list[User]:
+        return await get_users_list(info.context["db"])
+
+
+user_schema = strawberry.Schema(UserQuery, UserMutation)
 user_graphql_router = GraphQLRouter(user_schema, context_getter=get_db_contex)
--- graphql_template/users/services.py
+++ graphql_template/users/services.py
@@ -8,3 +8,8 @@ async def create_user(user: UserCreate, db: Database) -> None:
     user_query = users.insert().values(name=user.name)
     nonblock(db.execute, user_query)
     return None
+
+
+async def get_users_list(db: Database):
+    users_query = users.select()
+    return await db.fetch_all(users_query)
