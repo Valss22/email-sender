bb211e3387cd6d299dc46338f71249c656e5bb85
Valss22 <valsshokorov@gmail.com>
2022-12-10 20:10:39 +0600

    Fix import naming & remove unused requirement

--- app/users/services.py
+++ app/users/services.py
@@ -1,15 +1,15 @@
 from app.users.model import users
 from databases import Database
-from app.users.schemas import UserOut, CreateUserIn, UpdateUserIn
+from app.users.schemas import GetUser, CreateUser, UpdateUser
 
 
-async def create_user(user: CreateUserIn, db: Database) -> None:
+async def create_user(user: CreateUser, db: Database) -> None:
     user_query = users.insert().values(**user.__dict__)
     await db.execute(user_query)
     return None
 
 
-async def update_user(user: UpdateUserIn, db: Database) -> None:
+async def update_user(user: UpdateUser, db: Database) -> None:
     user_query = users.update().where(users.c.id == user.id).values(**user.__dict__)
     await db.execute(user_query)
     return None
@@ -21,7 +21,7 @@ async def delete_user(id: int, db: Database) -> None:
     return None
 
 
-async def get_user_by_id(id: int, db: Database) -> UserOut:
+async def get_user_by_id(id: int, db: Database) -> GetUser:
     user_query = users.select().where(users.c.id == id)
     return await db.fetch_one(user_query)
 
